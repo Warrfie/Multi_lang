e5f9d760d5871fdce6e2ac49525ab4e844641eb8
Warrfie <warrfie@gmail.com>
2022-02-16 18:12:16 +0300

    Initial commit

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    assert browser.find_elements(By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')
+
